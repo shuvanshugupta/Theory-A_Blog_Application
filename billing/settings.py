ff1facc7655a9770e39955088387e459f9894867
shuvanshugupta <52127401+shuvanshugupta@users.noreply.github.com>
2020-07-20 16:17:08 +0530

    Update settings.py

--- billing/settings.py
+++ billing/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
